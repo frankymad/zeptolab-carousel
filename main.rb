80f83e714a537b5be59d2dcd3e0cc32b673db225
frankymad <dmitry.kretsu@gmail.com>
2016-11-21 00:55:26 +0300

    Sinatra app + layouts

--- main.rb
+++ main.rb
@@ -1,10 +1,24 @@
 require 'open-uri'
 require 'json'
+require 'sinatra'
+require "rubygems"
 
 url = "https://web-assets.zeptolab.com/api/test_gallery.php"
 json_object = JSON.parse(open(url).read)
 
+get "/" do
+  @objects = json_object
+  erb :index
+end
+
+not_found do
+  status 404
+  erb :page404
+end
+
+=begin
 puts json_object.to_s
 puts json_object[0].to_s
 puts json_object[0].keys.to_s
 puts json_object[0]['name'].to_s
+=end
