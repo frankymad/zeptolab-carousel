40a4f0be9c49eb0cfc154c177b4507dba8e238b8
frankymad <dmitry.kretsu@gmail.com>
2016-11-22 01:39:23 +0300

    Three items on page

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,21 @@
  * Created by frankymad on 21.11.16.
 */
 var currentItem = 0;
+var playing = true;
 var interval = setInterval(nextItem,2500);
 
 function nextItem() {
-    items[currentItem].className = 'item';
+    items[currentItem].className = 'item prev';
     currentItem = (currentItem+1)%items.length;
     items[currentItem].className = 'item show';
+    items[(currentItem+1)%items.length].className = 'item next';
+    if (currentItem == 0)
+        {items[items.length-2].className = 'item';}
+    else if (currentItem == 1)
+        {items[items.length-1].className = 'item';}
+    else
+        {items[(currentItem-2)%items.length].className = 'item';}
 }
-
-var playing = true;
 
 function pauseSlideshow() {
     playing = false;
@@ -19,7 +25,7 @@ function pauseSlideshow() {
 
 function playSlideshow() {
     playing = true;
-    interval = setInterval(nextItem,2000);
+    interval = setInterval(nextItem,2500);
 }
 
 function playPause() {
